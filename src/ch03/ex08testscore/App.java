4f9104eed505ab04c511a437e1208f13c4a4e0d6
lecture team <team@newlecture.com>
2024-04-17 09:29:31 +0900

    성적관리 프로그램 과제 풀이

--- src/ch03/ex08testscore/App.java
+++ src/ch03/ex08testscore/App.java
@@ -16,9 +16,12 @@ public class App {
             System.out.println("└───────────────────────────────┘");
             System.out.println();
             System.out.println();
-            System.out.println();
+            System.out.println();            
+        }
+        {
             System.out.println("─────────────────────────────────");
             System.out.println("Press any Key to Continue>");
+
             Scanner scan = new Scanner(System.in);
             scan.nextLine();
         }
@@ -30,7 +33,36 @@ public class App {
                 System.out.println("┌───────────────────────────────┐");
                 System.out.println("│        Test Score List        │");
                 System.out.println("└───────────────────────────────┘");
-               
+                FileInputStream fis = new FileInputStream("res/exams.csv");        
+                Scanner scan = new Scanner(fis);
+
+                if(scan.hasNextLine())        
+                    scan.nextLine();
+
+                System.out.printf("%-4s %-4s %-4s %-4s %-6s %-6s\n",
+                                "NO", "KOR","ENG","MATH","TOTAL","AVG");
+                
+                // int total
+                int index=0;
+                while(scan.hasNextLine())
+                {
+                    String line = scan.nextLine();
+                    
+                    String[] tokens = line.split(",");//["10","90","50"];
+                    
+                    int kor = Integer.parseInt(tokens[0]);
+                    int eng = Integer.parseInt(tokens[1]);
+                    int math = Integer.parseInt(tokens[2]);
+                    int total = kor+eng+math;
+                    double avg = total/3.0;
+        
+                    System.out.printf("%-4d %-4d %-4d %-4d %-6d %-6.2f\n", 
+                                index+1, kor,eng,math,total,avg);
+                
+                    index++;
+                }                
+            }
+            {
                 System.out.println("─────────────────────────────────");
                 System.out.println("Press any Key to Continue>");
 
@@ -41,61 +73,38 @@ public class App {
             // Test Score Input
             clear();
             {
-                int kor = 1;
-                int eng = 2;
+                System.out.println("┌───────────────────────────────┐");
+                System.out.println("│        Test Score Input       │");
+                System.out.println("└───────────────────────────────┘");
 
+                Scanner scan = new Scanner(System.in);
+
+                System.out.print("Kor:");
+                int kor = Integer.parseInt(scan.nextLine());
+                System.out.print("Eng:");
+                int eng = Integer.parseInt(scan.nextLine());
+                System.out.print("Math:");
+                int math = Integer.parseInt(scan.nextLine());
+                
                 FileOutputStream fos = new FileOutputStream("res/exams.csv",true);
                 PrintStream out = new PrintStream(fos);
 
+                out.printf("\n%d,%d,%d",kor, eng, math);
 
+                out.close();
+                fos.close();
+
+            }
+            {
                 System.out.println("─────────────────────────────────");
                 System.out.println("Press any Key to Continue>");
+
                 Scanner scan = new Scanner(System.in);
                 scan.nextLine();
-
             }
         }
 
 
-        /*
-        FileInputStream fis = new FileInputStream("res/exams.csv");        
-        Scanner scan = new Scanner(fis);
-        
-
-
-        if(scan.hasNextLine())        
-            scan.nextLine();
-
-        out.println("KOR,ENG,MATH,TOTAL,AVG");
-
-        int index=0;
-        // int total
-        while(scan.hasNextLine())
-        {
-            String line = scan.nextLine();
-            
-            String[] tokens = line.split(",");//["10","90","50"];
-            
-            int kor = Integer.parseInt(tokens[0]);
-            int eng = Integer.parseInt(tokens[1]);
-            int math = Integer.parseInt(tokens[2]);
-            int total = kor+eng+math;
-            double avg = total/3.0;
-
-            out.printf("%d,%d,%d,%d,%.2f", kor,eng,math,total,avg);
-
-            if(scan.hasNextLine())
-                out.println();
-
-            // index++;
-        }
-                
-
-        scan.close();
-        fis.close();
-        // out.close();
-        // fos.close();
-         */
     }
 
     public static void clear() {
