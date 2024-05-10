8384ace3287380b58060bdaf04e2fef7ed84277f
lecture team <team@newlecture.com>
2024-05-10 17:09:32 +0900

    인위적인 반복

---
+++ src/ch03/ex13limit/App.java
@@ -0,0 +1,12 @@
+package ch03.ex13limit;
+
+public class App {
+    public static void main(String[] args) {
+        
+        int i=0;
+        while(i<23){
+            System.out.println("hello");
+            i++;
+        }
+    }
+}
